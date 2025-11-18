f7994dd9417451d7807cded76694ff47c90b8cfe
Salis Qodri Mufti Muhammad <salis.qodri@gmail.com>
2025-11-18 10:13:19 +0700

    update db

--- database.sql
+++ database.sql
@@ -6,6 +6,9 @@
 SET time_zone = "+07:00";
 SET FOREIGN_KEY_CHECKS=0;
 
+CREATE DATABASE IF NOT EXISTS `Hotel_Reservation` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `Hotel_Reservation`;
+
 -- --------------------------------------------------------
 -- 1. Tabel Guests (Tamu)
 -- Menyimpan data unik tamu
